91860b964f9dc7b87d11b88f9fb41eb586a301f4
ayueh0822 <ayueh0822@gmail.com>
2017-12-08 21:28:25 +0800

    add wget
    add wget to hw4_test.sh
    download rnn model and embedding files

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+wget 'https://github.com/ayueh0822/ML2017FALL/releases/download/0.0.1/embedding'
+wget 'https://github.com/ayueh0822/ML2017FALL/releases/download/0.0.1/embedding.syn1neg.npy'
+wget 'https://github.com/ayueh0822/ML2017FALL/releases/download/0.0.1/embedding.wv.syn0.npy'
+wget 'https://github.com/ayueh0822/ML2017FALL/releases/download/0.0.1/model.hdf5'
+
 python3 test.py $1 $2
